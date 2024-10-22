a8f855f66562752f20f8c3c070f3377a3f4cabb9
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2024-10-22 12:22:41 +0700

    style(Button): add filled and non-filled component style
    CU-86b2k8jkf

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -11,28 +11,47 @@ const Button = ({
   className,
   disabled = false,
   fullWidth = false,
+  filled = false,
   ...props
 }) => {
+  
   const baseClasses = twMerge(
     "inline-flex items-center rounded-xl gap-[6px] border focus:outline-none font-semibold justify-center",
     fullWidth ? "w-full" : "w-fit"
   );
-  const variantClasses = {
+
+  const outlineClasses = {
     primary:
-      "bg-primary-100 hover:bg-primary-80 focus:bg-primary-110 focus:border-primary-100 text-white",
+      "text-primary-100 border-primary-100 hover:text-primary-80 hover:border-primary-80 focus:text-primary-110 focus:border-primary-110",
     white:
-      "bg-white hover:bg-fade hover:border-fade focus:bg-white border-gray-20 focus:border-0 text-gray-50",
-    info: "bg-info-90 hover:bg-info-80 focus:bg-info-100 focus:border-info-90 text-white",
+      "text-gray-20 border-gray-20 hover:text-fade hover:border-fade focus:text-gray-10 focus:border-0",
+    info: "text-info-90 border-info-90 hover:text-info-80 hover:border-info-80 focus:text-info-100 focus:border-info-100",
     success:
-      "bg-success-90 hover:bg-success-80 focus:bg-success-100 focus:border-success-90 text-white",
+      "text-success-90 border-success-90 hover:text-success-80 hover:border-success-80 focus:text-success-100 focus:border-success-100",
     danger:
-      "bg-danger-90 hover:bg-danger-80 focus:bg-danger-100 focus:border-danger-90 text-white",
+      "text-danger-90 border-danger-90 hover:text-danger-80 hover:border-danger-80 focus:text-danger-100 focus:border-danger-100",
     warning:
-      "bg-warning-90 hover:bg-warning-80 focus:bg-warning-100 focus:border-warning-90 text-white",
+      " text-warning-90 border-warning-90 hover:text-warning-80 hover:border-warning-80 focus:text-warning-100 focus:border-warning-100",
   };
 
-  const disabledClasses =
-    "disabled:bg-gray-30 disabled:text-gray-40 disabled:cursor-not-allowed";
+  const filledClasses = {
+    primary:
+      "bg-primary-100 text-white hover:bg-primary-80 focus:bg-primary-110",
+    white:
+      "bg-white text-gray-50 hover:bg-fade hover:border-fade focus:bg-white",
+    info: "bg-info-90 text-white hover:bg-info-80 focus:bg-info-100",
+    success:
+      "bg-success-90 text-white hover:bg-success-80 focus:bg-success-100",
+    danger: "bg-danger-90 text-white hover:bg-danger-80 focus:bg-danger-100",
+    warning:
+      "bg-warning-90 text-white hover:bg-warning-80 focus:bg-warning-100",
+  };
+
+  const disabledClasses = `disabled:text-gray-40 disabled:cursor-not-allowed ${
+    filled
+      ? "disabled:bg-gray-30"
+      : "disabled:bg-transparent disabled:border-gray-40"
+  }`;
 
   const sizeClasses = {
     sm: "text-[13px] py-1.5 px-3",
@@ -40,6 +59,7 @@ const Button = ({
     lg: "text-sm py-3 px-4",
   };
 
+  const typeClasses = filled ? filledClasses[variant] : outlineClasses[variant];
   return (
     <button
       type={type}
@@ -47,10 +67,11 @@ const Button = ({
       disabled={disabled}
       className={twMerge(
         baseClasses,
-        variantClasses[variant],
+        typeClasses,
         sizeClasses[size],
         disabled && disabledClasses,
-        className
+        className,
+        filled
       )}
       {...props}
     >
