787d6273953183d78d666fd36e8bf56d8c76af72
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2024-11-05 11:36:21 +0700

    feat(Toast): add show Toast function
    CU-86b2rb3d5

--- src/components/Toast/index.jsx
+++ src/components/Toast/index.jsx
@@ -1,74 +1,80 @@
 "use client";
-import { useState } from "react";
-import { twMerge } from "tailwind-merge";
-import { X } from "@phosphor-icons/react";
-import { IconButton, Text } from "..";
+import {useEffect, useState} from "react";
+import {twMerge} from "tailwind-merge";
+import {X} from "@phosphor-icons/react";
+import {IconButton, Text} from "..";
 
 const Toast = ({
-  title = "New Toast",
-  message = "This is a new alert",
-  status = "success",
-  position = "top-right",
-  dismissable = true,
-  ...props
-}) => {
-  const [isVisible, setIsVisible] = useState(true);
+                   title = "New Toast",
+                   message = "This is a new alert",
+                   status = "success",
+                   position = "top-right",
+                   dismissable = true,
+                   isVisible,
+                   onDismiss,
+                   ...props
+               }) => {
+    const [isAnimating, setIsAnimating] = useState(isVisible);
 
-  const closeToast = () => {
-    setIsVisible(false);
-  };
-  const baseClasses =
-    "flex flex-row gap-4 p-4 rounded-xl w-72 border items-start transition-opacity duration-300 ease-in-out shadow-[0px_16px_22px_-6px_rgba(16,24,40,0.1)]";
+    useEffect(() => {
+        if (isVisible) {
+            setIsAnimating(true);
+        } else {
+            const timer = setTimeout(() => {
+                setIsAnimating(false);
+            }, 300);
 
-  const successClasses = "border-success-60 bg-success-10 text-success-90";
+            return () => clearTimeout(timer);
+        }
+    }, [isVisible]);
 
-  const warningClasses = "border-warning-60 bg-warning-10 text-warning-90";
+    const baseClasses =
+        "flex flex-row gap-4 p-4 rounded-xl w-[296px] border justify-between items-start transition-opacity duration-300 ease-in-out shadow-[0px_16px_22px_-6px_rgba(16,24,40,0.1)]";
 
-  const dangerClasses = "border-danger-60 bg-danger-10 text-danger-90";
+    const statusClasses = {
+        success: "border-success-60 bg-success-10 text-success-90",
+        danger: "border-danger-60 bg-danger-10 text-danger-90",
+        warning: "border-warning-60 bg-warning-10 text-warning-90",
+        info: "border-info-60 bg-info-10 text-info-90",
+    };
 
-  const infoClasses = "border-info-60 bg-info-10 text-info-90";
+    const positionClasses = {
+        "top-right": "top-44 right-9",
+        "top-left": "top-44 left-9",
+        "bottom-right": "bottom-12 right-9",
+        "bottom-left": "bottom-12 left-9",
+    };
 
-  const statusClasses = {
-    success: successClasses,
-    danger: dangerClasses,
-    warning: warningClasses,
-    info: infoClasses,
-  };
-
-  const positionClasses = {
-    "top-right": "top-12 right-9",
-    "top-left": "top-12 left-9",
-    "bottom-right": "bottom-12 right-9",
-    "bottom-left": "bottom-12 left-9",
-  };
-  return (
-    <div
-      className={twMerge("absolute z-50", positionClasses[position])}
-      {...props}
-    >
-      <div
-        className={twMerge(
-          baseClasses,
-          statusClasses[status],
-          isVisible ? "opacity-100" : "opacity-0"
-        )}
-      >
-        <div className="flex flex-col gap-1">
-          <Text size="sm" weight="600">
-            {title}
-          </Text>
-          <Text size="xs" color="text-gray-50" weight="600">
-            {message}
-          </Text>
+    return (
+        <div
+            className={twMerge("absolute z-50", positionClasses[position])}
+            {...props}
+        >
+            <div
+                className={twMerge(
+                    baseClasses,
+                    statusClasses[status],
+                    isAnimating
+                        ? (isVisible ? 'animate-fade-in' : 'animate-fade-out') // Add animation classes
+                        : 'opacity-0'
+                )}
+            >
+                <div className="flex flex-col gap-1">
+                    <Text size="sm" weight="600">
+                        {title}
+                    </Text>
+                    <Text size="xs" color="text-gray-50" weight="400">
+                        {message}
+                    </Text>
+                </div>
+                {dismissable && (
+                    <IconButton onClick={onDismiss}>
+                        <X weight="bold" size={12}/>
+                    </IconButton>
+                )}
+            </div>
         </div>
-        {dismissable && (
-          <IconButton onClick={closeToast}>
-            <X weight="bold" size={12} />
-          </IconButton>
-        )}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Toast;
