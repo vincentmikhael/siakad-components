2c05077904db305f97ca8bf430a4c7d2423b734a
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2024-11-20 18:27:21 +0700

    feat(Kurikulum) : update kurikulum views
    CU-86b2wgbe2

--- src/views/data-utama/Kurikulum.jsx
+++ src/views/data-utama/Kurikulum.jsx
@@ -1,10 +1,9 @@
 "use client"
 
-import {useEffect, useState} from "react";
-import AxiosInstance from "@/libs/AxiosInstance";
+import {useState} from "react";
 import {
-    Button, FileUpload, Hr, IconButton,
-    Input, Label, Modal, Select,
+    Button, IconButton,
+    Input, Modal, Select,
     Table,
     TableBody,
     TableBodyCell,
@@ -16,25 +15,22 @@ import {
 import {MagnifyingGlass, Plus, PencilSimpleLine, Trash} from "@phosphor-icons/react";
 
 const Kurikulum = () => {
-    const [openAddModal, setOpenAddModal] = useState(false);
-    const [openEditModal, setOpenEditModal] = useState(false);
-    const [openDeleteModal, setOpenDeleteModal] = useState(false);
-    const handleOpenAddModal = () => setOpenAddModal(true);
-    const handleCloseAddModal = () => setOpenAddModal(false);
-    const handleOpenEditModal = () => setOpenEditModal(true);
-    const handleCloseEditModal = () => setOpenEditModal(false);
-    const handleOpenDeleteModal = () => setOpenDeleteModal(true);
-    const handleCloseDeleteModal = () => setOpenDeleteModal(false);
+    const [activeModal, setActiveModal] = useState(null);
+
+    const handleOpenModal = (modal) => setActiveModal(modal);
+
+    const handleCloseModal = () => setActiveModal(null);
+
     const columns = [
-        {name: "no", minWidth: "min-w-14"},
-        {name: "kode mk", minWidth: "min-w-[100px]"},
-        {name: "nama mata kuliah", minWidth: "min-w-[260px]"},
-        {name: "semester kurikulum", minWidth: "min-w-[174px]"},
-        {name: "sifat", minWidth: "min-w-[120px]"},
-        {name: "sks", minWidth: "min-w-[60px"},
-        {name: "tahun kurikulum", minWidth: "min-w-[150px]"},
-        {name: "user entri", minWidth: "min-w-44"},
-        {name: "actions", minWidth: "min-w-32"},
+        {name: "no", className: "min-w-14"},
+        {name: "kode mk", className: "min-w-[100px] text-center"},
+        {name: "nama mata kuliah", className: "min-w-[260px]"},
+        {name: "semester kurikulum", className: "min-w-[174px] text-center"},
+        {name: "sifat", className: "min-w-[120px] text-center"},
+        {name: "sks", className: "min-w-[60px] text-center"},
+        {name: "tahun kurikulum", className: "min-w-[150px] text-center"},
+        {name: "user entri", className: "min-w-[170px] text-center"},
+        {name: "actions", className: "min-w-[124px]  text-center"},
     ];
     const pinnedColumns = [0, 1, 2]
     const data = [
@@ -78,7 +74,7 @@ const Kurikulum = () => {
                         leftIcon={<MagnifyingGlass weight="bold"/>}
                     />
                     <Button
-                        onClick={handleOpenAddModal}
+                        onClick={() => handleOpenModal("add")}
                         leftIcon={<Plus weight="bold"/>}
                         size="sm"
                         filled
@@ -99,7 +95,7 @@ const Kurikulum = () => {
                         {columns.map((e, index) => {
                             return (
                                 <TableHeadCell
-                                    className={e.minWidth}
+                                    className={e.className}
                                     key={index}
                                 >
                                     {e.name}
@@ -117,33 +113,34 @@ const Kurikulum = () => {
                                     <Text size="xs">{index + 1}</Text>
                                 </TableBodyCell>
                                 <TableBodyCell>
-                                    <Text size="xs">{e?.kode}</Text>
+                                    <Text size="xs" className="text-center">{e?.kode}</Text>
                                 </TableBodyCell>
                                 <TableBodyCell>
                                     <Text size="xs" className="uppercase">{e?.mk}</Text>
                                 </TableBodyCell>
                                 <TableBodyCell>
-                                    <Text size="xs">{e?.semester}</Text>
+                                    <Text size="xs" className="text-center">{e?.semester}</Text>
                                 </TableBodyCell>
                                 <TableBodyCell>
-                                    <Text size="xs">{e?.sifat}</Text>
+                                    <Text size="xs" className="text-center">{e?.sifat}</Text>
                                 </TableBodyCell>
                                 <TableBodyCell>
-                                    <Text size="xs">{e?.sks}</Text>
+                                    <Text size="xs" className="text-center">{e?.sks}</Text>
                                 </TableBodyCell>
                                 <TableBodyCell>
-                                    <Text size="xs">{e?.tahun}</Text>
+                                    <Text size="xs" className="text-center">{e?.tahun}</Text>
                                 </TableBodyCell>
                                 <TableBodyCell>
-                                    <Text size="xs">{e?.user}</Text>
+                                    <Text size="xs" className="text-center">{e?.user}</Text>
                                 </TableBodyCell>
 
                                 <TableBodyCell>
-                                    <div className="flex flex-row gap-3">
-                                        <IconButton size="sm" variant="warning" onClick={handleOpenEditModal}>
+                                    <div className="flex flex-row gap-3 justify-center">
+                                        <IconButton size="sm" variant="warning" onClick={() => handleOpenModal("edit")}>
                                             <PencilSimpleLine/>
                                         </IconButton>
-                                        <IconButton size="sm" variant="danger" onClick={handleOpenDeleteModal}>
+                                        <IconButton size="sm" variant="danger"
+                                                    onClick={() => handleOpenModal("delete")}>
                                             <Trash/>
                                         </IconButton>
                                     </div>
@@ -157,8 +154,8 @@ const Kurikulum = () => {
             {/*Add Modal*/}
             <Modal
                 size="lg"
-                open={openAddModal}
-                onClose={handleCloseAddModal}
+                open={activeModal === "add"}
+                onClose={handleCloseModal}
                 title="Tambah data kurikulum"
                 dismissable
                 autoClose
@@ -216,8 +213,8 @@ const Kurikulum = () => {
             {/*Edit Modal*/}
             <Modal
                 size="lg"
-                open={openEditModal}
-                onClose={handleCloseEditModal}
+                open={activeModal === "edit"}
+                onClose={handleCloseModal}
                 title="Perbarui data kurikulum"
                 dismissable
                 autoClose
@@ -264,7 +261,7 @@ const Kurikulum = () => {
                 <Modal.Footer>
                     <div className="gap-4 flex flex-row">
                         <Button variant="primary" size="md" disabled filled>
-                            Tambah
+                            Perbarui
                         </Button>
                         <Button variant="white" size="md" filled>
                             Batal
@@ -274,8 +271,8 @@ const Kurikulum = () => {
             </Modal>
             {/*Delete Modal*/}
             <Modal
-                open={openDeleteModal}
-                onClose={handleCloseDeleteModal}
+                open={activeModal === "delete"}
+                onClose={handleCloseModal}
                 title="Hapus data kurikulum"
                 description="Apakah Anda yakin ingin menghapus data ini? Tindakan ini tidak dapat dibatalkan."
                 dismissable
