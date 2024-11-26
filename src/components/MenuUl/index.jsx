02d34a3ee956b979f1805c84ad24b98587cef9c1
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2024-11-26 15:40:35 +0700

    style(Menu) : update Menu width
    CU-86b2jjtgw

--- src/components/MenuUl/index.jsx
+++ src/components/MenuUl/index.jsx
@@ -1,12 +1,13 @@
-import { twMerge } from "tailwind-merge";
-const MenuUl = ({ children, className, ...props }) => {
-  const baseClasses =
-    "flex xl:flex-col flex-row gap-2 xl:max-w-[100%*0,18333333] max-w-full overflow-x-auto xl:overflow-x-clip scrollbar-none xl:border-0 border-b border-fade pb-6 mb-6";
-  return (
-    <ul className={twMerge(baseClasses, className, "over")} {...props}>
-      {children}
-    </ul>
-  );
+import {twMerge} from "tailwind-merge";
+
+const MenuUl = ({children, className, ...props}) => {
+    const baseClasses =
+        "flex xl:flex-col flex-row gap-2 xl:max-w-[100%*0,18333333] min-w-[264px] max-w-full overflow-x-auto xl:overflow-x-clip scrollbar-none xl:border-0 border-b border-fade pb-6 mb-6";
+    return (
+        <ul className={twMerge(baseClasses, className)} {...props}>
+            {children}
+        </ul>
+    );
 };
 
 export default MenuUl;
