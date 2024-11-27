1d38aac8c2097375f1ad288523007f9af74865de
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2024-11-27 10:51:01 +0700

    feat(Tabs) : update Tab Component render item
    CU-86b2zmz98

--- src/components/Tabs/TabItem.jsx
+++ src/components/Tabs/TabItem.jsx
@@ -1,3 +1,4 @@
+"use client"
 import {twMerge} from 'tailwind-merge';
 
 const TabItem = ({
@@ -8,11 +9,9 @@ const TabItem = ({
                      children,
                      ...restProps
                  }) => {
-    const baseClasses = "inline-block text-sm leading-[22.4px] p-3 font-semibold text-center disabled:cursor-not-allowed"
-
-    const activeClasses = "text-primary-100 border-b-2 border-primary-100"
-
-    const inactiveClasses = "text-gray-50"
+    const baseClasses = "inline-block text-sm leading-[22.4px] p-3 font-semibold text-center disabled:cursor-not-allowed";
+    const activeClasses = "text-primary-100 border-b-2 border-primary-100";
+    const inactiveClasses = "text-gray-50";
 
     const buttonClass = twMerge(
         className,
@@ -33,5 +32,6 @@ const TabItem = ({
             </button>
         </li>
     );
-}
+};
+
 export default TabItem;
--- src/components/Tabs/index.jsx
+++ src/components/Tabs/index.jsx
@@ -1,15 +1,11 @@
-'use client'
+'use client';
 import React, {useState} from 'react';
 import {twMerge} from 'tailwind-merge';
+import {Button, Input} from "@/components";
+import {MagnifyingGlass, Plus} from "@phosphor-icons/react";
 
-const Tabs = ({
-                  defaultClass = 'flex flex-wrap gap-2 border-b border-fade w-fit',
-                  className,
-                  children,
-              }) => {
-
+const Tabs = ({defaultClass = 'flex flex-wrap gap-2 border-b border-fade w-fit', className, children}) => {
     const [activeTab, setActiveTab] = useState(0);
-
     return (
         <>
             <ul className={twMerge(defaultClass, className)}>
@@ -21,11 +17,30 @@ const Tabs = ({
                     })
                 )}
             </ul>
+            <div className="flex gap-4 w-full md:w-fit">
+                <Input
+                    size="xs"
+                    className="w-full lg:w-[156px]"
+                    placeholder={"Cari data disini"}
+                    leftIcon={<MagnifyingGlass weight="bold"/>}
+                />
+                <Button
+                    onClick={() => {
+                    }}
+                    leftIcon={<Plus weight="bold"/>}
+                    size="sm"
+                    filled
+                    className="w-full lg:w-fit"
+                >
+                    Tambah data
+                </Button>
+            </div>
+
             <div className="pt-6">
-                {children[activeTab]?.props.children}
+                {React.Children.toArray(children)[activeTab]?.props.children}
             </div>
         </>
     );
-}
+};
 
 export default Tabs;
