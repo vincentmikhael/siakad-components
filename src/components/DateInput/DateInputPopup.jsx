4777640e05cb80af82531a2e2a0f40f08166bd92
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2024-12-03 20:25:59 +0700

    style(DateInput): change pop up date and size input
    CU-86b31vuma

--- src/components/DateInput/DateInputPopup.jsx
+++ src/components/DateInput/DateInputPopup.jsx
@@ -1,42 +1,44 @@
-import { Button, Input, Text } from "..";
-import { twMerge } from "tailwind-merge";
+import {Button, Input, Text} from "..";
+import {twMerge} from "tailwind-merge";
 import DateInputControl from "./DateInputControl";
 
-const Week = ["Mo", "Tu", "We", "Th", "Fr", "Sat", "Su"];
-
-const positionClasses = {
-  "bottom-right": "top-10 right-0",
-  "bottom-left": "top-10 left-0",
-  "top-right": "bottom-10 right-0",
-  "top-left": "bottom-10 left-0",
-};
-
 const DateInputPopup = ({
-  position = "bottom-right",
-  currentMonth,
-  currentYear,
-  navigateMonth,
-  children,
-  dateToday,
-  clearDate,
-  actionButton,
-}) => {
-  return (
-    <div
-      className={twMerge(
-        "absolute my-1.5 z-30 block w-full max-w-[328px]",
-        positionClasses[position]
-      )}
-    >
-      <div className="inline-block rounded-xl bg-white custom-shadow-datepicker border border-fade">
-        <div className="px-6 py-5 space-y-3">
-          <DateInputControl
-            currentMonth={currentMonth}
-            currentYear={currentYear}
-            navigateToNextMonth={() => navigateMonth(1)}
-            navigateToPrevMonth={() => navigateMonth(-1)}
-          />
-          {/* <div className="flex gap-3">
+                            position = "bottom-right",
+                            currentMonth,
+                            currentYear,
+                            navigateMonth,
+                            children,
+                            dateToday,
+                            clearDate,
+                            actionButton,
+                            size
+                        }) => {
+
+    const Week = ["Mo", "Tu", "We", "Th", "Fr", "Sat", "Su"];
+
+    const positionClasses = {
+        "bottom-right": `${size === "sm" ? "top-[72px]" : "top-20"} right-0`,
+        "bottom-left": `${size === "sm" ? "top-[72px]" : "top-20"} left-0`,
+        "top-right": `${size === "sm" ? "bottom-12" : "bottom-14"} right-0`,
+        "top-left": `${size === "sm" ? "bottom-12" : "bottom-14"} left-0`,
+    };
+
+    return (
+        <div
+            className={twMerge(
+                "absolute z-30 block w-full max-w-[328px]",
+                positionClasses[position]
+            )}
+        >
+            <div className="inline-block rounded-xl bg-white custom-shadow-datepicker border border-fade">
+                <div className="px-6 py-5 space-y-3">
+                    <DateInputControl
+                        currentMonth={currentMonth}
+                        currentYear={currentYear}
+                        navigateToNextMonth={() => navigateMonth(1)}
+                        navigateToPrevMonth={() => navigateMonth(-1)}
+                    />
+                    {/* <div className="flex gap-3">
             <Input className="w-full" />
             <Button
               variant="white"
@@ -46,38 +48,38 @@ const DateInputPopup = ({
               Today
             </Button>
           </div> */}
-          <div className="grid grid-cols-7 gap-y-1 gap-x-0">
-            {Week.map((day) => (
-              <div
-                className="w-10 h-10 items-center flex justify-center"
-                key={day}
-              >
-                <Text tag="span" weight="600" size="sm">
-                  {day}
-                </Text>
-              </div>
-            ))}
-            {children}
-          </div>
+                    <div className="grid grid-cols-7 gap-y-1 gap-x-0">
+                        {Week.map((day) => (
+                            <div
+                                className="w-10 h-10 items-center flex justify-center"
+                                key={day}
+                            >
+                                <Text tag="span" weight="600" size="sm">
+                                    {day}
+                                </Text>
+                            </div>
+                        ))}
+                        {children}
+                    </div>
+                </div>
+                {actionButton && (
+                    <div className="p-4 gap-3 flex flex-row border-t border-fade">
+                        <Button className="w-full" size="md" onClick={dateToday}>
+                            Today
+                        </Button>
+                        <Button
+                            className="w-full"
+                            size="md"
+                            variant="white"
+                            onClick={clearDate}
+                        >
+                            Clear
+                        </Button>
+                    </div>
+                )}
+            </div>
         </div>
-        {actionButton && (
-          <div className="p-4 gap-3 flex flex-row border-t border-fade">
-            <Button className="w-full" size="md" onClick={dateToday}>
-              Today
-            </Button>
-            <Button
-              className="w-full"
-              size="md"
-              variant="white"
-              onClick={clearDate}
-            >
-              Clear
-            </Button>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default DateInputPopup;
--- src/components/DateInput/index.jsx
+++ src/components/DateInput/index.jsx
@@ -1,149 +1,169 @@
 "use client";
-import { useState, useEffect, useRef, useMemo } from "react";
 
+import {useState, useEffect, useRef, useMemo} from "react";
 import getDaysInMonth from "@/utils/getDaysInMonth";
 import formatDate from "@/utils/formatDate";
 import useClickOutside from "@/hooks/useClickOutside";
 import DateInputPopup from "./DateInputPopup";
 import DateItem from "./DateItem";
-import { CalendarBlank } from "@phosphor-icons/react";
-import { Input } from "..";
+import {CalendarBlank} from "@phosphor-icons/react";
+import {Input} from "..";
+import {twMerge} from "tailwind-merge";
 
 function getDateSlots(currentMonth, currentYear) {
-  const dateArray = getDaysInMonth(currentMonth, currentYear);
-  const slotSkipCount = new Date(dateArray[0]).getDay();
+    const dateArray = getDaysInMonth(currentMonth, currentYear);
+    const slotSkipCount = new Date(dateArray[0]).getDay();
 
-  for (let i = 0; i < slotSkipCount; i++) {
-    dateArray.unshift(null);
-  }
+    for (let i = 0; i < slotSkipCount; i++) {
+        dateArray.unshift(null);
+    }
 
-  return dateArray;
+    return dateArray;
 }
 
-const DateInput = ({ value, onChange, disabled, actionButton = false }) => {
-  const popupRef = useRef();
-  const [showPopup, setShowPopup] = useState(false);
-  const [selectedDate, setSelectedDate] = useState("");
-  const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
-  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
-  const [typingTimeout, setTypingTimeout] = useState(null);
+const DateInput = ({
+                       value,
+                       onChange,
+                       disabled,
+                       actionButton = false,
+                       size = "sm",
+                       placeholder = "Select date",
+                       label = "Label",
+                       showLabel = false,
+                       showHint = false,
+                       error,
+                       className,
+                       position
+                   }) => {
+    const popupRef = useRef();
+    const [showPopup, setShowPopup] = useState(false);
+    const [selectedDate, setSelectedDate] = useState("");
+    const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
+    const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+    const [typingTimeout, setTypingTimeout] = useState(null);
 
-  useClickOutside(popupRef, () => {
-    setShowPopup(false);
-  });
-
-  const dateArray = useMemo(
-    () => getDateSlots(currentMonth, currentYear),
-    [currentMonth, currentYear]
-  );
-
-  useEffect(() => {
-    if (value) {
-      const dateObj = new Date(value);
-
-      setSelectedDate(formatDate(dateObj));
-      setCurrentMonth(dateObj.getMonth());
-      setCurrentYear(dateObj.getFullYear());
-    }
-  }, [value]);
-
-  function togglePopupHandler() {
-    setShowPopup((currentShowPopup) => {
-      return !currentShowPopup;
+    useClickOutside(popupRef, () => {
+        setShowPopup(false);
     });
-  }
 
-  function navigateMonthHandler(navigateBy = 1) {
-    if (currentMonth + navigateBy === 12) {
-      setCurrentMonth(0);
-      setCurrentYear((currentState) => {
-        return currentState + 1;
-      });
-    } else if (currentMonth + navigateBy === -1) {
-      setCurrentMonth(11);
-      setCurrentYear((currentState) => {
-        return currentState - 1;
-      });
-    } else {
-      setCurrentMonth((currentState) => {
-        return currentState + navigateBy;
-      });
-    }
-  }
-
-  function selectDateHandler(date) {
-    onChange(new Date(date));
-    setSelectedDate(formatDate(date));
-    setShowPopup(false);
-  }
-
-  function clearDate() {
-    setSelectedDate(null);
-  }
-
-  function dateToday() {
-    setShowPopup(false);
-    setSelectedDate(formatDate(new Date()));
-  }
-
-  function handleInputChange(event) {
-    const inputValue = event.target.value;
-    setSelectedDate(inputValue);
-
-    if (typingTimeout) {
-      clearTimeout(typingTimeout);
-    }
-
-    setTypingTimeout(
-      setTimeout(() => {
-        const parsedDate = new Date(inputValue);
-        if (!isNaN(parsedDate) && inputValue === formatDate(parsedDate)) {
-          onChange(parsedDate);
-          setCurrentMonth(parsedDate.getMonth());
-          setCurrentYear(parsedDate.getFullYear());
-        }
-      }, 1000)
+    const dateArray = useMemo(
+        () => getDateSlots(currentMonth, currentYear),
+        [currentMonth, currentYear]
     );
-  }
 
-  return (
-    <span ref={popupRef}>
-      <div className="relative max-w-sm">
+    useEffect(() => {
+        if (value) {
+            const dateObj = new Date(value);
+
+            setSelectedDate(formatDate(dateObj));
+            setCurrentMonth(dateObj.getMonth());
+            setCurrentYear(dateObj.getFullYear());
+        }
+    }, [value]);
+
+    function togglePopupHandler() {
+        setShowPopup((currentShowPopup) => {
+            return !currentShowPopup;
+        });
+    }
+
+    function navigateMonthHandler(navigateBy = 1) {
+        if (currentMonth + navigateBy === 12) {
+            setCurrentMonth(0);
+            setCurrentYear((currentState) => {
+                return currentState + 1;
+            });
+        } else if (currentMonth + navigateBy === -1) {
+            setCurrentMonth(11);
+            setCurrentYear((currentState) => {
+                return currentState - 1;
+            });
+        } else {
+            setCurrentMonth((currentState) => {
+                return currentState + navigateBy;
+            });
+        }
+    }
+
+    function selectDateHandler(date) {
+        onChange(new Date(date));
+        setSelectedDate(formatDate(date));
+        setShowPopup(false);
+    }
+
+    function clearDate() {
+        setSelectedDate(null);
+    }
+
+    function dateToday() {
+        setShowPopup(false);
+        setSelectedDate(formatDate(new Date()));
+    }
+
+    function handleInputChange(event) {
+        const inputValue = event.target.value;
+        setSelectedDate(inputValue);
+
+        if (typingTimeout) {
+            clearTimeout(typingTimeout);
+        }
+
+        setTypingTimeout(
+            setTimeout(() => {
+                const parsedDate = new Date(inputValue);
+                if (!isNaN(parsedDate) && inputValue === formatDate(parsedDate)) {
+                    onChange(parsedDate);
+                    setCurrentMonth(parsedDate.getMonth());
+                    setCurrentYear(parsedDate.getFullYear());
+                }
+            }, 1000)
+        );
+    }
+
+    return (
+        <span ref={popupRef}>
+      <div className={twMerge("relative w-full", className)}>
         <Input
-          value={selectedDate}
-          onChange={handleInputChange}
-          onFocus={togglePopupHandler}
-          placeholder="Select date"
-          rightIcon={<CalendarBlank size={16} weight="bold" />}
-          disabled={disabled}
+            size={size}
+            placeholder={placeholder}
+            label={label}
+            showLabel={showLabel}
+            showHint={showHint}
+            error={error}
+            value={selectedDate}
+            onChange={handleInputChange}
+            onFocus={togglePopupHandler}
+            rightIcon={<CalendarBlank size={16} weight="bold"/>}
+            disabled={disabled}
         />
-
-        {showPopup && (
-          <DateInputPopup
-            currentMonth={currentMonth}
-            currentYear={currentYear}
-            navigateMonth={navigateMonthHandler}
-            clearDate={clearDate}
-            dateToday={dateToday}
-            actionButton={actionButton}
-          >
-            {dateArray.map((dateObj, index) => {
-              return (
-                <DateItem
-                  key={index}
-                  dateObj={dateObj.date}
-                  isFromPreviousMonth={dateObj.isFromPreviousMonth}
-                  isFromNextMonth={dateObj.isFromNextMonth}
-                  selected={selectedDate === formatDate(dateObj.date)}
-                  onClick={() => selectDateHandler(dateObj.date)}
-                />
-              );
-            })}
-          </DateInputPopup>
-        )}
+          {showPopup && (
+              <DateInputPopup
+                  currentMonth={currentMonth}
+                  currentYear={currentYear}
+                  navigateMonth={navigateMonthHandler}
+                  clearDate={clearDate}
+                  dateToday={dateToday}
+                  actionButton={actionButton}
+                  position={position}
+                  size={size}
+              >
+                  {dateArray.map((dateObj, index) => {
+                      return (
+                          <DateItem
+                              key={index}
+                              dateObj={dateObj.date}
+                              isFromPreviousMonth={dateObj.isFromPreviousMonth}
+                              isFromNextMonth={dateObj.isFromNextMonth}
+                              selected={selectedDate === formatDate(dateObj.date)}
+                              onClick={() => selectDateHandler(dateObj.date)}
+                          />
+                      );
+                  })}
+              </DateInputPopup>
+          )}
       </div>
     </span>
-  );
+    );
 };
 
 export default DateInput;
