fefe0a0f6f4351ee3c4ba67e96e4bcbba7f12d23
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2024-12-19 15:17:55 +0700

    style(Layout): change active nav li condition
    CU-86b2jjtgw

--- src/components/NavLi/index.jsx
+++ src/components/NavLi/index.jsx
@@ -1,39 +1,39 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-import { useEffect, useState, useContext } from "react";
-import { Link } from "..";
-import { NavbarContext } from "../Navbar";
+import {usePathname} from "next/navigation";
+import {useEffect, useState, useContext} from "react";
+import {Link} from "..";
+import {NavbarContext} from "../Navbar";
 
-const NavLi = ({ children, href, icon }) => {
-  const [active, setActive] = useState(false);
-  const pathname = usePathname();
-  const { setShowNav } = useContext(NavbarContext); 
+const NavLi = ({children, href, icon}) => {
+    const [active, setActive] = useState(false);
+    const pathname = usePathname();
+    const {setShowNav} = useContext(NavbarContext);
 
-  useEffect(() => {
-    setActive(pathname.includes(href));
-  }, [pathname, href]);
+    useEffect(() => {
+        setActive(pathname === href || pathname.startsWith(`${href}/`));
+    }, [pathname, href]);
 
-  const handleClick = () => {
-    setShowNav(false); // Tutup navbar saat link diklik
-  };
+    const handleClick = () => {
+        setShowNav(false);
+    };
 
-  return (
-    <li>
-      <Link
-        href={href}
-        onClick={handleClick}
-        className={`text-sm font-normal rounded-lg flex xl:block p-3 xl:py-1.5 gap-2 items-center ${
-          active
-            ? "bg-primary-100 text-gray-10 xl:bg-gray-10 xl:text-primary-100"
-            : "bg-gray-20 text-gray-70 xl:text-white xl:bg-transparent xl:px-0"
-        }`}
-      >
-        {icon && <span className="xl:hidden text-base">{icon}</span>}
-        {children}
-      </Link>
-    </li>
-  );
+    return (
+        <li>
+            <Link
+                href={href}
+                onClick={handleClick}
+                className={`text-sm font-normal rounded-lg flex xl:block p-3 xl:py-1.5 gap-2 items-center ${
+                    active
+                        ? "bg-primary-100 text-gray-10 xl:bg-gray-10 xl:text-primary-100"
+                        : "bg-gray-20 text-gray-70 xl:text-white xl:bg-transparent xl:px-0"
+                }`}
+            >
+                {icon && <span className="xl:hidden text-base">{icon}</span>}
+                {children}
+            </Link>
+        </li>
+    );
 };
 
 export default NavLi;
