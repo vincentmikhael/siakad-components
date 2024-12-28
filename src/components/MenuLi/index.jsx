b81dc9af6db82bdab1763ff31b1cd39b7f89b690
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2024-12-28 12:11:31 +0700

    style(Layout): change active menu li condition and add configuration tailwind
    CU-86b2jjtgw

--- src/components/MenuLi/index.jsx
+++ src/components/MenuLi/index.jsx
@@ -1,26 +1,27 @@
 "use client";
 import Link from "next/link";
-import { useState, useEffect } from "react";
-import { usePathname } from "next/navigation";
-const MenuLi = ({ children, href }) => {
-  const [active, setActive] = useState(false);
-  const pathname = usePathname();
-  useEffect(() => {
-    setActive(pathname.includes(href));
-  }, [pathname, href]);
-  return (
-    <li>
-      <Link
-        href={href}
-        className={`text-sm font-semibold rounded-xl flex flex-grow px-4 py-3 items-center whitespace-nowrap ${
-          active
-            ? "bg-primary-80 text-gray-10 custom-shadow-menu"
-            : "text-gray-100 bg-transparent border border-fade"
-        }`}
-      >
-        {children}
-      </Link>
-    </li>
-  );
+import {useState, useEffect} from "react";
+import {usePathname} from "next/navigation";
+
+const MenuLi = ({children, href}) => {
+    const [active, setActive] = useState(false);
+    const pathname = usePathname();
+    useEffect(() => {
+        setActive(pathname === href || pathname.startsWith(`${href}/`));
+    }, [pathname, href]);
+    return (
+        <li>
+            <Link
+                href={href}
+                className={`text-sm font-semibold rounded-xl flex flex-grow px-4 py-3 items-center whitespace-nowrap ${
+                    active
+                        ? "bg-primary-80 text-gray-10 custom-shadow-menu"
+                        : "text-gray-100 bg-transparent border border-fade"
+                }`}
+            >
+                {children}
+            </Link>
+        </li>
+    );
 };
 export default MenuLi;
