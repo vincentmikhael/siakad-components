463c45d3f5e4a0a83ff75f26fdb60053614ac37b
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2024-12-30 21:12:53 +0700

    fix(Radio): fix radio button function
    CU-86b3aytp7

--- src/components/Radio/index.jsx
+++ src/components/Radio/index.jsx
@@ -1,42 +1,57 @@
-import { twMerge } from "tailwind-merge";
+"use client"
+import {twMerge} from "tailwind-merge";
+import {useState} from "react";
 
 const Radio = ({
-  value,
-  className,
-  children,
-  checked,
-  onChange,
-  disabled = false,
-  ...restProps
-}) => {
-  return (
-    <label className={"flex flex-row gap-1.5 items-center"}>
-      <label className="relative flex items-center cursor-pointer">
-        <input
-          type="radio"
-          value={value}
-          checked={checked} // Ensure it reflects the checked state
-          onChange={onChange} // Handle state change
-          className={twMerge(
-            className,
-            "peer appearance-none w-4 h-4 border border-fade rounded-full checked:border-2 checked:border-primary-100 focus:custom-shadow-radio disabled:bg-fade disabled:cursor-not-allowed"
-          )}
-          disabled={disabled}
-          {...restProps}
-        />
-        <span className="absolute bg-primary-100 w-2 h-2 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
-      </label>
-      {children && (
-        <span
-          className={`font-medium text-sm ${
-            checked ? "text-gray-100" : "text-gray-50"
-          } transition-colors duration-200`}
-        >
-          {children}
-        </span>
-      )}
-    </label>
-  );
+                   options,
+                   selected,
+                   name,
+                   className,
+                   children,
+                   onChange,
+                   ...restProps
+               }) => {
+    const [optionSelected, setOptionSelected] = useState(selected || "");
+    const handleSelect = (value) => {
+        setOptionSelected(value);
+        if (onChange) onChange(value);
+    }
+    return (
+        // <label className={"flex flex-row gap-1.5 items-center"}>
+        <>
+            {options.map((option, index) => (
+                <div className={"flex flex-row gap-1.5 items-center cursor-pointer"} key={index}>
+                    <label className="relative flex items-center">
+                        <input
+                            type="radio"
+                            value={option.value}
+                            name={name}
+                            checked={optionSelected === option.value} // Ensure it reflects the checked state
+                            onChange={() => handleSelect(option.value)} // Handle state change
+                            className={twMerge(
+                                className,
+                                "peer appearance-none w-4 h-4 border border-fade rounded-full checked:border-2 checked:border-primary-100 focus:custom-shadow-radio disabled:bg-fade disabled:cursor-not-allowed"
+                            )}
+                            disabled={option.disabled}
+                            {...restProps}
+                        />
+                        <span
+                            className="absolute bg-primary-100 w-2 h-2 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"/>
+                    </label>
+                    {option.label && (
+                        <span
+                            className={`font-medium text-sm ${
+                                optionSelected === option.value ? "text-gray-100" : "text-gray-50"
+                            } transition-colors duration-200`}
+                        >
+                            {option.label}
+                        </span>
+                    )}
+                </div>
+            ))}
+            {/*</label>*/}
+        </>
+    );
 };
 
 export default Radio;
