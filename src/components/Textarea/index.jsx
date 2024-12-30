318e3ace2f01248b97c6ee169d1c0b1da81b9240
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2024-12-30 21:08:27 +0700

    style(Textarea): change placeholder font size
    CU-86b3ayt7p

--- src/components/Textarea/index.jsx
+++ src/components/Textarea/index.jsx
@@ -19,7 +19,7 @@ const Textarea = ({
     const errorClasses =
         "border-danger-90 shadow-[0_0_1px_3px_rgba(255,65,68,0.15)]";
     const baseClasses = twMerge(
-        "rounded-xl bg-white px-3.5 py-2.5 placeholder:text-gray-30 w-full text-gray-100 disabled:bg-fade disabled:cursor-not-allowed border resize-none",
+        "rounded-xl bg-white px-3.5 py-2.5 placeholder:text-gray-30 placeholder:text-sm w-full text-gray-100 disabled:bg-fade disabled:cursor-not-allowed border resize-none",
         error
             ? errorClasses
             : "border-gray-20 shadow-[0_1px_2px_0_rgba(16,24,40,0.05)] focus:border-primary-100 focus:shadow-[0_0_1px_3px_rgba(6,140,205,0.15)]"
