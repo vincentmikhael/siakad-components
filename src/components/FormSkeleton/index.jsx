418a2e269851d98182c52fcdfb108604b37714b8
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2025-01-07 17:32:02 +0700

    style(FormSkeleton): add responsive condition
    CU-86b314f2h

--- src/components/FormSkeleton/index.jsx
+++ src/components/FormSkeleton/index.jsx
@@ -1,8 +1,8 @@
 import {twMerge} from "tailwind-merge";
 
-const FormSkeleton = ({count, className, cols} = 2) => {
+const FormSkeleton = ({count, className, cols = 2}) => {
     return (
-        <div className={`grid grid-cols-${cols} gap-6 flex-grow`}>
+        <div className={`grid grid-cols-1 sm:grid-cols-${cols} gap-6 flex-grow`}>
             {Array.from({length: count}).map((_, index) => (
                 <div key={index} className={twMerge("skeleton skeleton-box h-12", className)}></div>
             ))}
