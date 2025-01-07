4509410455767c35a4f6cc6f0f6a218dccaa667b
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2025-01-07 13:02:57 +0700

    feat(Pagination): add completely works pagination
    CU-86b3dybv7

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,30 +1,145 @@
-import { CaretLeft, CaretRight } from "@phosphor-icons/react/dist/ssr"
-import { Button } from ".."
+"use client"
+import {CaretLeft, CaretRight} from "@phosphor-icons/react/dist/ssr"
+import {Button, IconButton} from ".."
+import {useEffect, useState} from "react";
 
-const Pagination = () =>{
+const disabledButtonClass = "disabled:cursor-not-allowed disabled:text-gray-20 disabled:border-gray-20 disabled:bg-white"
+const Pagination = ({currentPage, totalPages, onPageChange}) => {
+    const [page, setPage] = useState(currentPage);
+    const handlePageClick = (page) => {
+        setPage(page);
+        onPageChange(page);
+    };
+
+    useEffect(() => {
+        setPage(currentPage);
+    }, [currentPage]);
+
+    const RenderPageNumbers = () => {
+        const maxVisiblePages = 5;
+        let startPage, endPage;
+
+        if (totalPages <= maxVisiblePages) {
+            startPage = 1;
+            endPage = totalPages;
+        } else {
+            if (page <= 3) {
+                startPage = 1;
+                endPage = 3;
+            } else if (page >= totalPages - 2) {
+                startPage = totalPages - 2;
+                endPage = totalPages;
+            } else {
+                startPage = page;
+                endPage = page;
+            }
+        }
+
+        const pageNumbers = Array.from({length: endPage - startPage + 1}, (_, i) => startPage + i);
+
+        return (
+            <div className="flex space-x-2">
+                {startPage > 1 && (
+                    <>
+                        <Button
+                            key={1}
+                            filled
+                            variant="white"
+                            className={`w-9 h-9 ${page !== 1 ? "border-0" : ""}`}
+                            onClick={() => handlePageClick(1)}
+                        >
+                            1
+                        </Button>
+                        {startPage > 2 && <Button
+                            filled
+                            variant="white"
+                            className="w-9 h-9 border-0"
+                        >
+                            ...
+                        </Button>}
+                    </>
+                )}
+
+                {pageNumbers.map((p) => (
+                    <Button
+                        key={p}
+                        filled
+                        variant="white"
+                        className={`w-9 h-9 ${p !== page ? "border-0" : ""}`}
+                        onClick={() => handlePageClick(p)}
+                    >
+                        {p}
+                    </Button>
+                ))}
+
+                {endPage < totalPages && (
+                    <>
+                        {endPage < totalPages - 1 &&
+                            <Button
+                                filled
+                                variant="white"
+                                className="w-9 h-9 border-0"
+                            >
+                                ...
+                            </Button>}
+                        <Button
+                            key={totalPages}
+                            filled
+                            variant="white"
+                            className={`w-9 h-9 ${page !== totalPages ? "border-0" : ""}`}
+                            onClick={() => handlePageClick(totalPages)}
+                        >
+                            {totalPages}
+                        </Button>
+                    </>
+                )}
+            </div>
+        );
+    };
+
     return (
-        <div className="flex justify-between items-center mt-6">
-          <nav aria-label="Pagination" className="flex items-center w-full">
-            <div className="flex-1 flex justify-start">
-              <Button variant="white" leftIcon={<CaretLeft />}>
-                Previous
-              </Button>
-            </div>
-            <div className="flex-1 flex justify-center gap-3">
-              <Button variant="white">1</Button>
-              <Button variant="white" className={"border-0"}>
-                2
-              </Button>
-              <Button variant="white" className={"border-0"}>
-                3
-              </Button>
-            </div>
-            <div className="flex-1 flex justify-end">
-              <Button variant="white" rightIcon={<CaretRight />}>
-                Next
-              </Button>
-            </div>
-          </nav>
+        <div className="flex justify-between items-center md:px-6 pt-3 pb-4">
+            <nav aria-label="Pagination" className="flex items-center justify-between gap-3 w-full">
+                <Button
+                    variant="white"
+                    leftIcon={<CaretLeft weight="bold"/>}
+                    size="sm"
+                    filled
+                    disabled={page === 1}
+                    onClick={() => handlePageClick(page - 1)}
+                    className={`hidden lg:flex ${page === 1 && disabledButtonClass}`}>
+                    Previous
+                </Button>
+
+                <IconButton
+                    className={`flex w-9 h-9 lg:hidden ${page === 1 && disabledButtonClass}`}
+                    disabled={page === 1}
+                    onClick={() => handlePageClick(page - 1)}>
+                    <CaretLeft weight="bold"/>
+                </IconButton>
+
+                <div className="flex justify-center items-center gap-0.5">
+                    <RenderPageNumbers/>
+                </div>
+
+                <IconButton
+                    className={`flex w-9 h-9 lg:hidden ${page === totalPages && disabledButtonClass}`}
+                    disabled={page === totalPages}
+                    onClick={() => handlePageClick(page + 1)}>
+                    <CaretRight weight="bold"/>
+                </IconButton>
+
+                <Button
+                    variant="white"
+                    rightIcon={<CaretRight weight="bold"/>}
+                    size="sm"
+                    filled
+                    disabled={page === totalPages}
+                    onClick={() => handlePageClick(page + 1)}
+                    className={`hidden lg:flex ${page === totalPages && disabledButtonClass}`}>
+                    Next
+                </Button>
+            </nav>
         </div>
     )
 }
