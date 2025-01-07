4fb087af3cffdd95290faeddb06f10d2023e2ca4
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2025-01-07 12:54:13 +0700

    style(IconButton): change variant styling and rearrange classes
    CU-86b2pu8hj

--- src/components/IconButton/index.jsx
+++ src/components/IconButton/index.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const IconButton = ({
                         children,
-                        className,
+                        className = "",
                         size = "sm",
                         type = "button",
                         disabled = false,
@@ -20,38 +20,27 @@ const IconButton = ({
         md: "rounded-xl text-base",
     };
 
+    const disabledClasses = size === "sm"
+        ? "disabled:bg-gray-20 disabled:border-fade"
+        : "disabled:bg-gray-30 disabled:border-0"
+
+
     const variantClasses = {
-        primary: `bg-primary-10 text-primary-100 border-primary-30 hover:border-primary-80 hover:text-primary-80 focus:border-primary-90 focus:text-primary-90 focus:bg-primary-20 disabled:text-gray-40 ${
-            size === "sm"
-                ? "disabled:bg-gray-20 disabled:border-fade"
-                : "disabled:bg-gray-30 disabled:border-0"
-        }`,
-        white:
-            "bg-white text-gray-40 border-gray-20 hover:border-fade disabled:border-0 disabled:bg-gray-30",
-        warning: `bg-warning-10 text-warning-90 border-warning-30 hover:border-warning-80 hover:text-warning-80 focus:border-warning-90 focus:text-warning-90 focus:bg-warning-20 ${
-            size === "sm"
-                ? "disabled:bg-gray-20 disabled:border-fade"
-                : "disabled:bg-gray-30 disabled:border-0"
-        }`,
-        danger: `bg-danger-10 text-danger-90 border-danger-30 hover:border-danger-80 hover:text-danger-80 focus:border-danger-90 focus:text-danger-90 focus:bg-danger-20 ${
-            size === "sm"
-                ? "disabled:bg-gray-20 disabled:border-fade"
-                : "disabled:bg-gray-30 disabled:border-0"
-        }`,
-        success: `bg-success-10 text-success-90 border-success-30 hover:border-success-80 hover:text-success-80 focus:border-success-90 focus:text-success-90 focus:bg-success-20 ${
-            size === "sm"
-                ? "disabled:bg-gray-20 disabled:border-fade"
-                : "disabled:bg-gray-30 disabled:border-0"
-        }`,
+        primary: `bg-primary-10 text-primary-100 border-primary-30 hover:border-primary-80 hover:text-primary-80 focus:border-primary-90 focus:text-primary-90 focus:bg-primary-20`,
+        white: "bg-white text-gray-40 border-gray-20 hover:border-fade",
+        warning: `bg-warning-10 text-warning-90 border-warning-30 hover:border-warning-80 hover:text-warning-80 focus:border-warning-90 focus:text-warning-90 focus:bg-warning-20`,
+        danger: `bg-danger-10 text-danger-90 border-danger-30 hover:border-danger-80 hover:text-danger-80 focus:border-danger-90 focus:text-danger-90 focus:bg-danger-20`,
+        success: `bg-success-10 text-success-90 border-success-30 hover:border-success-80 hover:text-success-80 focus:border-success-90 focus:text-success-90 focus:bg-success-20`,
     };
 
     if (href) {
         return (
             <Link href={href} className={twMerge(
                 baseClasses,
-                className,
                 sizeClasses[size],
-                variantClasses[variant]
+                variantClasses[variant],
+                disabledClasses,
+                className,
             )} {...props}
             >{children}</Link>
         )
@@ -63,9 +52,10 @@ const IconButton = ({
             disabled={disabled}
             className={twMerge(
                 baseClasses,
-                className,
                 sizeClasses[size],
-                variantClasses[variant]
+                variantClasses[variant],
+                disabledClasses,
+                className,
             )}
             {...props}
         >
