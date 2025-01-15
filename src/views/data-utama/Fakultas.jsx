36bb488da91f1a65abf166812c392850678f9141
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2025-01-15 23:14:35 +0700

    style(Fakultas): disable button when submit
    CU-86b2ha19q

--- src/views/data-utama/Fakultas.jsx
+++ src/views/data-utama/Fakultas.jsx
@@ -510,8 +510,12 @@ const Fakultas = () => {
                 </Modal.Body>
                 <Modal.Footer>
                     <div className="gap-4 flex flex-row">
-                        <Button variant="primary" size="md" filled onClick={handleSubmit} disabled={isFormIncomplete}>
-                            {loadingSubmit ? <Spinner size={16}/> : editMode ? "Perbarui" : "Tambah"}
+                        <Button variant="primary" size="md" filled onClick={handleSubmit}
+                                disabled={isFormIncomplete || loadingSubmit}>
+                            {loadingSubmit ? <>
+                                {editMode ? "Memperbarui" : "Menyimpan"}
+                                <Spinner size={16}/>
+                            </> : editMode ? "Perbarui" : "Tambah"}
                         </Button>
                         <Button variant="white" size="md" filled onClick={closeModal}>
                             Batal
